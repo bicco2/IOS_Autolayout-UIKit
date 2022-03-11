61ebf3cf6cc5d6da0c84355ddd7acdeb849b7bda
jinhobyeong <jhb_0921@naver.com>
2022-03-11 23:52:42 +0900

    study_22.3.11

--- CustomPopUp_tutorial/CustomPopUp_tutorial/ViewController.swift
+++ CustomPopUp_tutorial/CustomPopUp_tutorial/ViewController.swift
@@ -27,7 +27,7 @@ class ViewController: UIViewController {
         let storyboard = UIStoryboard.init(name: "PopUp", bundle: nil)
         
         //스토리보드를 통해 뷰컨트롤러 가져오기(스토리보드의 아이디를 설정해서 가져오는 것임)
-        let customPopUpVC = storyboard.instantiateViewController(withIdentifier: "AlertPopUpVC") as! CustomPopUpViewController
+        let customPopUpVC = storyboard.instantiateViewController(withIdentifier: "AlertPopUpVC") as! CustomPopUpViewController //as 는 타입 캐스팅임 (자료형 확정 )
         
         //팝업 효과 설정(이거 안하면 기본 팝업 설정효과로 됨) ==뷰 컨트롤러가 보여지는 스타일
         customPopUpVC.modalPresentationStyle = .overCurrentContext
@@ -36,7 +36,7 @@ class ViewController: UIViewController {
         customPopUpVC.modalTransitionStyle = .crossDissolve
         
         customPopUpVC.subscribeBtnCompletionClosure = {
-            print("컴플레션 블럭이 호출되었다. ")
+            print("컴플레션 블럭(클로져)이 호출되었다. ")
             let myChannelUrl = URL(string: "https://bicco2.tistory.com/")
             self.myWebView.load(URLRequest(url: myChannelUrl!))
         }
