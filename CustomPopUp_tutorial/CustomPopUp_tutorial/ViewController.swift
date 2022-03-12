dcbd89c7bf1a570f9a68503de3a95fc6a9f90165
jinhobyeong <jhb_0921@naver.com>
2022-03-12 14:21:33 +0900

    study_22.3.12

--- CustomPopUp_tutorial/CustomPopUp_tutorial/ViewController.swift
+++ CustomPopUp_tutorial/CustomPopUp_tutorial/ViewController.swift
@@ -12,18 +12,26 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var createPopUpBtn: UIButton!
     @IBOutlet weak var myWebView: WKWebView!
-     
+    @IBOutlet weak var greet: UILabel!
+    @IBOutlet weak var backToHomeBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //제스처 뒤로가기 활성화
+        self.myWebView?.allowsBackForwardNavigationGestures = true
+        
     }
 
+    
     
     @IBAction func onCreatePopUpBtnClicked(_ sender: UIButton) {
         
         print("ViewController - onCreatePopUpBtnClicked() called")
         
-        //스토리 보드 가져오기
+        //스토리 보드 가져오기 초기화(popup 스토리보드임 )
         let storyboard = UIStoryboard.init(name: "PopUp", bundle: nil)
         
         //스토리보드를 통해 뷰컨트롤러 가져오기(스토리보드의 아이디를 설정해서 가져오는 것임)
@@ -35,10 +43,11 @@ class ViewController: UIViewController {
         //팝업 == 뷰 컨트롤러가 사라지는 스타일
         customPopUpVC.modalTransitionStyle = .crossDissolve
         
-        customPopUpVC.subscribeBtnCompletionClosure = {
+        customPopUpVC.subscribeBtnCompletionClosure = { //이게 호출됐다는건 중심이 메인스토리보드로 옮겨간걸 의미한다 .
             print("컴플레션 블럭(클로져)이 호출되었다. ")
             let myChannelUrl = URL(string: "https://bicco2.tistory.com/")
             self.myWebView.load(URLRequest(url: myChannelUrl!))
+            self.greet.isHidden = true
         }
         
         //위에서 설정한 alertPopUpVC 를 보여주도록 하는 코드
@@ -46,6 +55,9 @@ class ViewController: UIViewController {
         
         
         
+    }
+    @IBAction func onBackToHomeBtnClicked(_ sender: UIButton) {
+        self.greet.isHidden = false
     }
     
 }
